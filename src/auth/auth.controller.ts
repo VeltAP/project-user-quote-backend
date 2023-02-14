fd6847a7e9aa68859855df58a18d1dc0f30dbf0a
VeltAP <veltishcheva2000@gmail.com>
2023-02-14 11:17:30 +0100

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,94 +1,73 @@
 import {
     Body,
-    Controller, Get, Param,
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
     Post,
-    Res
-} from '@nestjs/common';
-import {AuthService} from "./auth.service";
-// import * as bcrypt from 'bcrypt';
-// import {UserService} from "../user/user.service";
-// import {JwtService} from "@nestjs/jwt";
-import {Response} from "express";
-import {SignUpDto} from "./dto/signup.dto";
-import {LoginDto} from "./dto/login.dto";
-
-@Controller()
-export class AuthController {
-
-    constructor(
-        private readonly authService: AuthService,
-        // private readonly userService: UserService,
-        // private jwtService: JwtService
-    ) {
-    }
-
+    Req,
+    Res,
+    UseGuards,
+    UseInterceptors,
+  } from '@nestjs/common'
+  import { GetCurrentUser } from 'src/decorators/get-current-user.decorator'
+  import { GetCurrentUserId } from 'src/decorators/get-current-user-id.decorator'
+  import { Public } from 'src/decorators/public.decorator'
+  import { User } from 'src/modules/user/user.entity'
+  import { Request, Response } from 'express'
+  import { RequestWithUser } from 'src/interfaces/auth.interface'
+  import { UserData } from 'src/interfaces/user.interface'
+  
+  import { AuthService } from './auth.service'
+  import { SignUpDto } from './dto/signup.dto'
+  import { JwtAuthGuard } from './guards/jwt.guard'
+  import { JwtRefreshAuthGuard } from './guards/jwt-refresh.guard'
+  import { LocalAuthGuard } from './guards/local-auth.guard'
+  
+  @Controller('auth')
+  @UseInterceptors(ClassSerializerInterceptor)
+  export class AuthController {
+    constructor(private authService: AuthService) {}
+  
+    @Public()
     @Post('signup')
-    async signup(@Body() newUser: SignUpDto) {
-        return this.authService.createNewUser(newUser);
+    @HttpCode(HttpStatus.CREATED)
+    async register(@Body() body: SignUpDto): Promise<User> {
+      return this.authService.register(body)
     }
-
-
+  
+    @Public()
+    @UseGuards(LocalAuthGuard)
     @Post('login')
-    async login(@Body() loggedUser: LoginDto,
-                @Res({passthrough: true}) response: Response) {
-        return this.authService.login(loggedUser, response)
+    @HttpCode(HttpStatus.OK)
+    async login(@Req() req: RequestWithUser, @Res() res: Response): Promise<void> {
+      return this.authService.login(req.user, res)
     }
-
-    @Post('logout')
-    async logout(@Res({passthrough: true}) response: Response) {
-        response.clearCookie('jwt');
-
-        return {
-            message: 'Success'
-        };
+  
+    @UseGuards(JwtAuthGuard)
+    @Post('signout')
+    @HttpCode(HttpStatus.OK)
+    async signout(@GetCurrentUserId() userId: string, @Res() res: Response): Promise<void> {
+      return this.authService.signout(userId, res)
     }
-
-    @Get('user/:id')
-    async getUser(@Param() params) {
-        const user = await this.authService.findUserById(params.userId);
-
-        if (!user) {
-            return { notFound: 'user not found' };
+  
+    @UseGuards(JwtRefreshAuthGuard)
+    @Post('refresh')
+    @HttpCode(HttpStatus.ACCEPTED)
+    async refreshTokens(@Req() req: Request): Promise<User> {
+      return this.authService.refreshTokens(req)
+    }
+  
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    async getCurrentUser(@GetCurrentUser() user: User): Promise<UserData> {
+      return {
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
         }
-
-        // const quote = await this.authService.findUserQuote(params.quoteId);
-        //
-        // if (!quote) {
-        //     return { notFound: 'user not post quote yet' };
-        // }
-
-        return {
-            // quoteId: quote.id,
-            // text: quote.text,
-            user: {
-                firstName: user.first_name,
-                lastName: user.last_name,
-                email: user.email,
-                userId: user.id,
-            }
-        };
     }
-
-    // @Get('user/:id')
-    // async findUserById(@Req() request: Request) {
-    //     try {
-    //         const cookie = request.cookies['jwt'];
-    //
-    //         const data = await this.jwtService.verifyAsync(cookie);
-    //
-    //         if (!data) {
-    //             throw new UnauthorizedException('User does not exist');
-    //         }
-    //
-    //         const user = await this.userService.findOne({id: data['id']});
-    //
-    //         const {password, ...result} = user
-    //
-    //         return result;
-    //
-    //     } catch (e) {
-    //         throw new UnauthorizedException('User does not exist');
-    //     }
-    // }
-
-}
+  }
